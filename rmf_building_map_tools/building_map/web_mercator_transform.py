9f87379dfb272fa01c939ed7e4a9b35b40f7084b
Morgan Quigley <morgan@osrfoundation.org>
2022-03-15 14:45:05 +0800

    Rebase from 'upstream'

--- rmf_building_map_tools/building_map/web_mercator_transform.py
+++ rmf_building_map_tools/building_map/web_mercator_transform.py
@@ -7,7 +7,6 @@ class WebMercatorTransform:
     """Transforms between Web Mercator points and transverse mercator planes"""
 
     def __init__(self, crs_name):
-        # crs_4326 = CRS.from_epsg(4326)  # also known as WGS84...
         print(f'WebMercatorTransform({crs_name})')
         self.crs_name = crs_name
         self.offset = (0, 0)
@@ -26,20 +25,14 @@ class WebMercatorTransform:
         meters_east = R * math.pi * (p[0] - 128) / 128
         # because our legacy code is all in image coordinates, the Y coord
         # is negated... this step will flip it back
+        # TODO: revisit this now that loading is not always flipped...
         meters_north = R * math.pi * (128 + p[1]) / 128
 
         (lat, lon) = \
             self.web_mercator_to_wgs84.transform(meters_east, meters_north)
 
-        # now we need to choose a TM plane. In the future we should be more
-        # generic, but for now let's start by using SVY21
         (tm_northing, tm_easting) = \
             self.web_mercator_to_tm.transform(meters_east, meters_north)
-
-        # crs_tm = CRS.from_epsg(3414)
-        # tm_false_easting = crs_tm.to_dict()['x_0']
-        # tm_false_northing = crs_tm.to_dict()['y_0']
-        # print(f'offset: {tm_false_easting}, {tm_false_northing}')
 
         tm_easting -= self.offset[0]
         tm_northing -= self.offset[1]
