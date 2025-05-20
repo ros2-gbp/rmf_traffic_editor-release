2e7abab2c6df7dc3910959f7e080b6e0624c710c
Yadunund <yadunund@gmail.com>
2025-05-20 18:26:13 +0000

    Rebase from 'upstream'

--- rmf_building_map_tools/building_map_generator/_init_argparse.py
+++ rmf_building_map_tools/building_map_generator/_init_argparse.py
@@ -16,7 +16,14 @@ shared_parser.add_argument("OUTPUT_WORLD", type=str,
                            help="Name of the .world file to output")
 shared_parser.add_argument("OUTPUT_MODEL_DIR", type=str,
                            help="Path to output the map model files")
-
+shared_parser.add_argument("--TEMPLATE_WORLD_FILE", type=str, default="",
+                           help="Specify the template for"
+                           + " the base simulation.")
+shared_parser.add_argument("--SKIP_CAMERA_POSE", action="store_true",
+                           help="Skips calculating and setting the initial "
+                           + "camera view pose. This flag should only be used "
+                           + "if the template SDF file already has the camera "
+                           + "pose defined.")
 # Create subparsers for Gazebo and Nav generation
 gazebo_parser = subparsers.add_parser(
     'gazebo',
